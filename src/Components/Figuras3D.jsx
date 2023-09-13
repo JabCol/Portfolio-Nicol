bff87fd0e524001c8a390732bbdbab76ebf0a05e
Nicol Ortiz <nicolvaleria0919@gmail.com>
2023-09-12 21:40:58 -0500

    Adding file .gitignore

---
+++ src/Components/Figuras3D.jsx
@@ -0,0 +1,18 @@
+// import { ConeGeometry, TorusGeometry } from "three"; // Importa las geometrías necesarias
+
+// const Figuras3D = () => {
+//   return (
+//         <>
+//             <mesh className="figuras" position={[3,2,2]}>
+//                 <coneGeometry args={[0.3, 2, 16]} /> 
+//                 <meshStandardMaterial color="blue" />
+//             </mesh>
+//             <mesh className="figuras" position={[3,0,2]}>
+//                 <torusGeometry /> 
+//                 <meshStandardMaterial color="orange" />
+//             </mesh>
+//         </>
+//   );
+// };
+
+// export default Figuras3D;
